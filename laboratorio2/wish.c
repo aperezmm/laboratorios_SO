c23b835283953f20db5a143d8eeb214e649716ef
aperezmm <alejandro.perez1@udea.edu.co>
2021-05-05 23:01:50 -0500

    printf deleted

--- laboratorio2/wish.c
+++ laboratorio2/wish.c
@@ -39,7 +39,6 @@ struct SplittedResponse
 
 builtin_command str_to_command(char *strcommand)
 {
-    // printf("strcommand [%s]", strcommand);
     int builtin_command_quantity = 3;
     for (int i = 0; i < builtin_command_quantity; i++)
     {
@@ -48,20 +47,13 @@ builtin_command str_to_command(char *strcommand)
             return commands[i].command;
         }
     }
-    // printf("not_found");
     return not_found;
 }
 
 void handle_error(char *c)
 {
-    // printf("--> {%s}\n", c);
     char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message));
-}
-
-int actual_directory()
-{
-    // printf("%s\n", getcwd(my_path, MAX_SIZE));
 }
 
 int is_empty_line(char *line)
@@ -89,8 +81,6 @@ int change_directory(struct SplittedResponse splitted_command)
     {
         char *route = splitted_command.data + (50);
         int result = chdir(route);
-
-        actual_directory();
 
         if (result == -1)
         {
@@ -144,10 +134,6 @@ void str_replace(char *target, const char *needle, const char *replacement)
 
 struct SplittedResponse split_command_argument(char *command, char *delimiter, int show)
 {
-    // if (show == 1)
-    // {
-    //     printf("split_command_argument command --[%s] delimiter --[%s]\n", command, delimiter);
-    // }
 
     char *data_splitted = malloc(5000 * sizeof(char *));
     int numOfArguments = 0;
@@ -163,20 +149,11 @@ struct SplittedResponse split_command_argument(char *command, char *delimiter, i
         char *s = tok;
         if (*s != '\0')
         {
-            if (show == 1)
-            {
-                //printf("TOK [%s]\n", tok);
-            }
             strcpy((data_splitted + (numOfArguments * 50)), tok);
             numOfArguments++;
         }
 
         tok = end;
-    }
-
-    if (show == 1)
-    {
-        //printf("response.data [%s]\n", data_splitted);
     }
 
     struct SplittedResponse response;
@@ -202,20 +179,10 @@ int is_command_with_redirection(struct SplittedResponse splitted_command)
         }
     }
 
-    // printf("COMANDO --> ");
-    // for (int i = 0; i < splitted_command.size; i++)
-    // {
-    //     printf("%s ", splitted_command.data + (i * 50));
-    // }
-    // printf("\n");
-
-    // printf("redirection_symbol_count [%d]\n", redirection_symbol_count);
-
     //VALIDAMOS QUE EXISTA SOLO UN > Y ESTE EN LA POSICIÓN PENULTIMA
     if (redirection_symbol_count == 1)
     {
         int index = splitted_command.size - 2;
-        //printf("index_data [%s]\n", splitted_command.data+(index*50));
         int is_equal = strcmp(splitted_command.data + (index * 50), ">");
         if (is_equal == 0)
         {
@@ -238,54 +205,39 @@ int is_command_with_redirection(struct SplittedResponse splitted_command)
 
 int run_command_with_params(char *binary_path, struct SplittedResponse splitted_command)
 {
-    //TO DO: p1.sh y p2.sh error aquí.
     int is_redirection = is_command_with_redirection(splitted_command);
-    // printf("is_redirection: %d\n", is_redirection);
 
     // redirection with error.
     if (is_redirection == -1)
     {
         // handle_error("Redirección con error");
-        //printf("REDIRECCION CON ERROR {%s}\n", binary_path);
         return -1;
     }
 
-    // int response = 0;
-
-    // TODO: verify if it's posible to do it easier.
     if (is_redirection == 1)
     {
-        //printf("REDIRECCION VALIDA {%s}\n", binary_path);
         close(STDOUT_FILENO);
         int output_index = splitted_command.size - 1;
         char *output_name = splitted_command.data + (output_index * 50);
         open(output_name, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
 
-        //printf(" name --> {%s}\n", output_name);
-
         char *args[splitted_command.size - 2];
 
         for (int i = 0; i < splitted_command.size - 2; i++)
         {
-            //printf("ARG[%d] = [%s]\n", i, splitted_command.data+(i*50));
             args[i] = strdup(splitted_command.data + (i * 50));
         }
         args[splitted_command.size - 2] = NULL;
         int resp = execv(binary_path, args);
-        //printf("RESPONSE TO RETURN {%d}\n", resp);
         return resp;
     }
     else
     {
         char *args[splitted_command.size + 1];
         args[0] = strdup(splitted_command.data);
-        // args[0] = strdup("ls");
-
-        //printf("Comando -- [%s]", splitted_command.data);
 
         for (int i = 1; i < splitted_command.size; i++)
         {
-            //printf("ARG[%d] = [%s]\n", i, splitted_command.data+(i*50));
             args[i] = strdup(splitted_command.data + (i * 50));
         }
         args[splitted_command.size] = NULL;
@@ -328,17 +280,11 @@ int update_path(struct SplittedResponse splitted_command)
         }
         strcpy(path_directory + ((splitted_command.size - 1) * 40), "NULL");
     }
-
-    /*
-    for(int i=0; i< splitted_command.size; i++){
-        printf("PATH DIRECTORY %s\n", path_directory+(i*40));
-    }*/
 }
 
 // Run a single command
 int run_command_in_path(struct SplittedResponse splitted_command)
 {
-    // printf("...run_command_in_path...\n");
     int i = 0;
     char pathToFile[MAX_SIZE];
 
@@ -350,16 +296,13 @@ int run_command_in_path(struct SplittedResponse splitted_command)
     {
         strcpy(pathToFile, (path_directory + (i * 40)));
         strcat(pathToFile, splitted_command.data);
-        //TO DO: Verificar que el archivo exista
         is_executable = access(pathToFile, X_OK);
-        //printf("is_executable -- {%s} -- {%d}\n", pathToFile, is_executable);
         i++;
     }
 
     if (is_executable != -1)
     {
         response = run_command_with_params(pathToFile, splitted_command);
-        //printf("run_command_with_params RESPONSE {%d}", response);
     }
 
     return response;
@@ -394,16 +337,7 @@ int execute_generic_command(char *generic_command)
     {
         pids[i] = 0;
     }
-
-    // esto es provicional....
-    int response = -365;
-
-    // printf("PARALLEL_COMMANDS.SIZE [%d]\n", parallel_commands.size);
-
-    // for (int i = 0; i < parallel_commands.size; i++)
-    // {
-    //     printf("PARALELO[%d] - {%s}\n", i, parallel_commands.data + (i * 50));
-    // }
+    int response = 0;
 
     for (int i = 0; i < parallel_commands.size; i++)
     {
@@ -413,25 +347,12 @@ int execute_generic_command(char *generic_command)
         single_command = split_command_argument(parallel_commands.data + (i * 50), " ", 0);
         builtin_command command = str_to_command(single_command.data);
 
-        // printf("COMANDO --> %d", command);
-
-        //IMPRIMIMOS EL ARRAY DE ELEMENTOS SEPARADOS POR EL CARACTER
-        // printf(" CONTENIDO DE PARALELO :\n");
-        // for (int i = 0; i < single_command.size; i++)
-        // {
-        //     printf("%s ", single_command.data + (i * 50));
-        // }
-        //printf("\n");
-
         if (pid < 0)
         {
             printf("Error lauching the process parellel\n");
         }
         else if (pid == 0)
         {
-            // CHILD PROCESS USED FOR NOT BUILT-IN COMMANDS ONLY.
-            // printf("EN HIJO {%s} {%d} \n", single_command.data, command );
-
             if (command == not_found)
             {
                 response = run_command_in_path(single_command);
@@ -447,8 +368,6 @@ int execute_generic_command(char *generic_command)
             // FATHER PROCESS
             if (command != not_found)
             {
-                // printf("built in {%d}", command );
-
                 switch (command)
                 {
                 case custom_exit:
@@ -482,19 +401,15 @@ int execute_generic_command(char *generic_command)
     for (int i = 0; i < parallel_commands.size; i++)
     {
         waitpid(pids[i], &status, 0);
-
-        // printf("RES [%d]\n", response);
     }
 }
 
 int execute_batch_mode(FILE *file)
 {
-    // printf("EXECUTING IN BATCH MODE\n");
     char line[MAX_SIZE_COMMAND];
     while (fgets(line, 1024, file))
     {
         char *token = line;
-        // printf("LINE --> [%s]", line);
         execute_generic_command(line);
     }
     exit(0);
